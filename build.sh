aed74cb4158b6f90f6d78fd5896d78f3341c7c8b
corka149 <corka149@mailbox.org>
2019-09-27 14:11:08 +0200

    Added Dockerfile and necessary scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t vision:latest -t vision:1.0.0 .
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vision',
+    version='1.0.0',
+    packages=['vision'],
+    url='https://github.com/corka149/vision',
+    license='MIT',
+    author='corka',
+    author_email='corka149@mailbox.org',
+    description='Visions task is to store temporary data in a relative secure way.'
+)
